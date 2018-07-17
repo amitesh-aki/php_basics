0018192f7d2dfbb850d33dd67d9a7a174271b2af
Your Name <amitesh.aki@gmail.com>
2018-07-17 20:32:00 +0530

    php_basics

---
+++ example.php
@@ -0,0 +1,17 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Boss</title>
+	<head>
+	<body>
+<h1>My bio<h1>
+<?php
+	
+echo "My name is Amitesh Kumar";
+
+ ?><br>
+<?php echo "Who is Amitesh?"; ?> <br>
+<?php echo "What is his age?"; ?> <br>
+<?php echo 10+10+6; ?>
+	</body>
+</html>
