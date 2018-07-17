138f11f3b27561282c4310ea619d6f0001d89b99
Your Name <amitesh.aki@gmail.com>
2018-07-17 21:18:44 +0530

    php_basics

---
+++ constant.php
@@ -0,0 +1,28 @@
+<!DOCTYPE>
+
+<html>
+		<head>
+				<title>Name</title>
+		</head>
+		<body>
+
+		<?phph ?>
+
+		<?php
+			$Name=45;
+
+			define("Value_of_Pi",3.14);
+
+			define("Value_of_Gravity",9.8);
+
+			echo "Pi value: " . Value_of_Pi; echo " <br>";
+			
+			echo "G Value: " . Value_of_Gravity;
+
+		?>
+
+		<?php
+		?>
+
+		</body>
+</html>
--- variables.php
+++ variables.php
@@ -6,17 +6,21 @@
 	<body>
 
 
-<?php
-$name = "Amitesh";
-echo $name; echo "<br>";
-$age = 22;
-echo $age; echo "<br";
-$a=3;
-echo $a;
-$b=6;
-$c=$a+$b;
-echo $c;
-
-?>
+	<?php
+		$name = "Amitesh";
+		echo $name; 
+		echo " <br>";
+		
+		$age = 22;
+		echo $age; 
+		echo " <br>";
+		
+		$a = 3;
+		echo $a;
+		
+		$b = 6;
+		$c = $a + $b;
+		echo $c;
+	?>
 	</body>
 </html>
